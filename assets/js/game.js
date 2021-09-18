613afe512aa496b51a84fefc944f3935646b7398
Leithen <leithen113@gmail.com>
2021-09-18 09:53:16 -0600

    Started creating basic variables and functions in JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an Alert! JavaScript is running!")
+var playerName = window.prompt("What is the name of your robot?")
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving comments");
+console.log(10 + 32);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
