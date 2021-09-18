156f8a09524887392f4c8d66f2643552b0926a62
Leithen <leithen113@gmail.com>
2021-09-18 12:43:36 -0600

    Added a break when the player dies

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,7 +73,15 @@ var fight = function(enemyName) {
 
 //Loop though each enemy
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
+    //If player dies, end game
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
