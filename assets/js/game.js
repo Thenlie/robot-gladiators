62706aed9df4d53f8974f1d188d5ddaa9680275e
Leithen <leithen113@gmail.com>
2021-09-19 11:11:35 -0600

    Removed playerName Null input bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -157,9 +157,18 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    console.log("Robot name: " + name);
+    return name;
+}
+
 //Player Stats
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
